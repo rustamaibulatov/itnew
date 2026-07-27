3a921a72e361dcf7495358fc756d9ba1d85b3da0
Rustam <aibulatov.1998@mail.ru>
2026-07-27 10:27:46 +0000

    Replace legacy apps with CRM foundation

--- itnew/settings.py
+++ itnew/settings.py
@@ -38,8 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'main',
-    'news',
+    'rest_framework',
+    'crm',
 ]
 
 MIDDLEWARE = [
