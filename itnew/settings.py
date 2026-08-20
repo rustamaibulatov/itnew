65668a0a68c3c21ce79c0a49b468b7a3c7733559
Rustam <aibulatov.1998@mail.ru>
2026-08-20 18:33:12 +0000

    Add API pagination

--- itnew/settings.py
+++ itnew/settings.py
@@ -126,3 +126,8 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 2,
+}
